fabb70c908f7d55068aa12cb49bc8a7b81f5a620
unknown <smisra@IN-DHL-SMISRA.comverse.com>
2014-10-16 13:08:41 +0530

    Modified user creation in mongodb

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -3,7 +3,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var crypto = require('crypto');
-var authTypes = ['github', 'twitter', 'facebook', 'google'];
+var authTypes = ['github', 'twitter', 'facebook', 'google','keystone'];
 
 var UserSchema = new Schema({
   name: String,
--- server/auth/local/passport.js
+++ server/auth/local/passport.js
@@ -2,79 +2,59 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var KeystoneStrategy = require('passport-keystone').Strategy;
 
-exports.setup = function (User, config) {
-/* passport.use(new LocalStrategy({
-      usernameField: 'email',
-      passwordField: 'password' // this is the virtual field on the model
-	
-    },
-    function(email, password, done) {
-      User.findOne({
-        email: email.toLowerCase()
-      }, function(err, user) {
-        if (err) return done(err);
+exports.setup = function(User, config) {
 
-        if (!user) {
-          return done(null, false, { message: 'This email is not registered.' });
-        }
-        if (!user.authenticate(password)) {
-          return done(null, false, { message: 'This password is not correct.' });
-        }
-        return done(null, user);
-      });
-    }
-  ));*/
-	
 	passport.use(new KeystoneStrategy({
-		usernameField: 'username',
-		passwordField: 'password', // this is the virtual field on the model
-	    //authUrl: 'https://identity.api.rackspacecloud.com',
-		authUrl: '10.230.22.160:5000',
-	    region: 'RegionOne',
-	    passReqToCallback : true // allows us to interact with req object
+		usernameField : 'username',
+		passwordField : 'password', // this is the virtual field on the model
+		//authUrl: 'https://identity.api.rackspacecloud.com',
+		authUrl : '10.230.22.160:5000',
+		region : 'RegionOne',
+		passReqToCallback : true
+	// allows us to interact with req object
 	}, function(req, identity, done) {
-	  if (!req.user) {
-	    /*var user = {
-	        id: identity.user.id,
-	        token: identity.token.id,
-	        username: identity.user.name,
-	        serviceCatalog: identity.raw.access.serviceCatalog
-	    };*/
-	    
-	     
-	      
-	      User.findOne({
-	          'name': identity.user.name
-	        }, function(err, user) {
-	          if (!user) {
-	        	  console.log('##USER NOT FOUND');
-	        	    user = new User({
-	            	name: identity.user.name,
-	 	            username: identity.user.username,
-	 	            role: identity.user.roles[0].name,
-	 	            provider: 'keystone'
-	            });
-	            user.save(function(err) {
-	              if (err) done(err);
-	              	return done(err, user);
-	            });
-	          } else {
-	        	  if(!user){
-	      	    	console.log('USER IS NULL');
-	      	    }
+		if (!req.user) {
 
-	      	    // Set session expiration to token expiration
-	      	    req.session.cookie.expires = Date.parse(identity.token.expires) - Date.now();
+			User.findOne({
+				'name' : identity.user.name
+			}, function(err, user) {
+				if (!user) {
+					console.log('##USER NOT FOUND');
+					user = new User({
+						name : identity.user.name,
+						username : identity.user.username,
+						role : identity.user.roles[0].name,
+						provider : 'keystone',
+						keystone : identity._json
+					});
+					user.save(function(err) {
 
-	      	    done(null, user);
-	        	  
-	          }
-	        });   
-	    
-	  } else {
-	      // user already exists
-	      var user = req.user; // pull the user out of the session
-	      return done(null, user);
-	  }
+						if (err) {
+							console.log('##ERROR' + err);
+							done(err);
+						}
+						return done(err, user);
+					});
+
+					// Set session expiration to token expiration
+					req.session.cookie.expires = Date
+							.parse(identity.token.expires)
+							- Date.now();
+					done(null, user);
+
+				} else {
+					// Set session expiration to token expiration
+					req.session.cookie.expires = Date
+							.parse(identity.token.expires)
+							- Date.now();
+					done(null, user);
+				}
+			});
+
+		} else {
+			// user already exists
+			var user = req.user; // pull the user out of the session
+			return done(null, user);
+		}
 	}));
 };
